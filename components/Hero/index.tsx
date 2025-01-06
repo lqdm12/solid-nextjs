f982b60fcc41171599b6e897b54d49506f2e5de1
Leonard Mendonça <lqdm12@gmail.com>
2025-01-06 18:49:48 -0400

    I changed the hero that part that has "plataforma totalmente focada na vunesp" and "seu futuro começa aqui. Prepare-se para conquistar a sua vaga."

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -19,16 +19,13 @@ const Hero = () => {
                 🔥 Solid - A Complete SaaS Web Template
               </h4>
               <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero ">
-                Free Next.js Template for {"   "}
+                Plataforma totalmente focada na {"   "}
                 <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark ">
-                  SaaS
+                  Vunesp
                 </span>
               </h1>
               <p>
-                Solid Pro - Packed with all the key integrations you need for
-                swift SaaS startup launch, including - Auth, Database, Sanity
-                Blog, Essential Components, Pages and More. Built-winth -
-                Next.js 13, React 18 and TypeScript.
+                Seu futuro começa aqui. Prepare-se para conquistar a sua vaga.
               </p>
 
               <div className="mt-10">
