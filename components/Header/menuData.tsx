6cba817168dc5c47869bf427122dbf3f15400c80
Leonard Mendonça <lqdm12@gmail.com>
2025-01-06 16:52:09 -0400

    testing

--- components/Header/menuData.tsx
+++ components/Header/menuData.tsx
@@ -3,13 +3,13 @@ import { Menu } from "@/types/menu";
 const menuData: Menu[] = [
   {
     id: 1,
-    title: "Home",
+    title: "Test",
     newTab: false,
     path: "/",
   },
   {
     id: 2,
-    title: "Features",
+    title: "test",
     newTab: false,
     path: "/#features",
   },
