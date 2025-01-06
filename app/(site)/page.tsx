e674563a881218222f6f8e773329212d0a3a2d43
Leonard Mendonça <lqdm12@gmail.com>
2025-01-06 19:51:13 -0400

    brand inactive

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from "next";
 import Hero from "@/components/Hero";
-import Brands from "@/components/Brands";
+// import Brands from "@/components/Brands";
 import Feature from "@/components/Features";
 import About from "@/components/About";
 import FeaturesTab from "@/components/FeaturesTab";
@@ -23,7 +23,6 @@ export default function Home() {
   return (
     <main>
       <Hero />
-      <Brands />
       <Feature />
       <About />
       <FeaturesTab />
--- components/Brands/index.tsx
+++ components/Brands/index.tsx
@@ -1,3 +1,4 @@
+/*
 "use client";
 import React from "react";
 import SingleBrand from "./SingleBrand";
@@ -6,7 +7,8 @@ import brandData from "./brandData";
 const Brands = () => {
   return (
     <>
-      {/* <!-- ===== Clients Start ===== --> */}
+      {/* <!-- ===== Clients Start ===== --> *///}
+      /*
       <section className="border border-x-0 border-y-stroke bg-alabaster py-11 dark:border-y-strokedark dark:bg-black">
         <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
           <div className="grid grid-cols-3 items-center justify-center gap-7.5 md:grid-cols-6 lg:gap-12.5 xl:gap-29">
@@ -16,9 +18,10 @@ const Brands = () => {
           </div>
         </div>
       </section>
-      {/* <!-- ===== Clients End ===== --> */}
-     </>
+      {/* <!-- ===== Clients End ===== --> *///}
+     /*
+      </>
    );
  };
-
- export default Brands;
+*/
+ // export default Brands;
