98c1ae8eca2f7da17085f1355681a56b5ed97c18
Leonard Mendonça <lqdm12@gmail.com>
2025-01-06 18:55:50 -0400

    empty part in the hero

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -16,8 +16,8 @@ const Hero = () => {
           <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
             <div className=" md:w-1/2">
               <h4 className="mb-4.5 text-lg font-medium text-black dark:text-white">
-                🔥 Solid - A Complete SaaS Web Template
-              </h4>
+              
+              </h4> {/* <!-- Empty  for while and above the "Plataforma" --> */}
               <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero ">
                 Plataforma totalmente focada na {"   "}
                 <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark ">
