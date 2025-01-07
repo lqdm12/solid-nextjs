fa2d09128bef283f41334c3b3e220066350d7ea1
Leonard Mendonça <lqdm12@gmail.com>
2025-01-07 01:00:44 -0400

    editing FAQ part

--- components/FAQ/faqData.tsx
+++ components/FAQ/faqData.tsx
@@ -3,18 +3,23 @@ import { FAQ } from "@/types/faq";
 const faqData: FAQ[] = [
   {
     id: 1,
-    quest: "Lorem ipsum dolor sit amet, consectetur",
-    ans: "We provide digital experience services to startups and small businesses. We help our clients succeed by creating brand identities, digital experiences.",
+    quest: "A plataforma é gratuita?",
+    ans: "Oferecemos conteúdos gratuitos, mas também temos opções premium que incluem materiais exclusivos, simulados avançados e relatórios de desempenho detalhados.",
   },
   {
     id: 2,
-    quest: "Lorem ipsum dolor sit amet, consectetur",
-    ans: "We provide digital experience services to startups and small businesses. We help our clients succeed by creating brand identities, digital experiences.",
+    quest: "Como posso acessar os materiais da NexusVest?",
+    ans: "Basta se cadastrar em nosso site e você terá acesso a diversos materiais gratuitos. Para acessar os recursos premium, é necessário adquirir um plano.",
   },
   {
     id: 3,
-    quest: "Lorem ipsum dolor sit amet, consectetur",
-    ans: "We provide digital experience services to startups and small businesses. We help our clients succeed by creating brand identities, digital experiences.",
+    quest: "Quais são as formas de pagamento para planos premium?",
+    ans: "Aceitamos diversas formas de pagamento, incluindo cartões de crédito, boletos bancários e transferências via plataformas de pagamento online.",
+  },
+  {
+    id: 4,
+    quest: "Como posso cancelar minha assinatura?",
+    ans: "Você pode cancelar sua assinatura a qualquer momento acessando sua conta na plataforma e seguindo as instruções para desativar sua assinatura. Não cobramos taxas de cancelamento.",
   },
 ];
 
